63c8ffaa31d583ed5e3d3b906f551edaba6b59fe
unknown <arjunkv321@gmail.com>
2023-04-04 15:32:23 +0530

    new

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
 
 const express = require('express')
-const mongoose = require('mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 // sndsd
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://arjun:Hack123@cluster0.res46s8.mongodb.net/?retryWrites=true&w=majority";
+
+let db;
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+  if (err) throw err;
+  db = client.db("rewards");
+});
+
 
 
 //Routes go here
-app.get('/', (req,res) => {
-    res.send({ title: 'Books' });
+app.get('/', (req,res) => { 
+  const collection = db.collection('accounts');
+  collection.find({}).toArray((err, data) => {
+    if (err) throw err;
+    res.send(data);
+  });
 })
 
 
